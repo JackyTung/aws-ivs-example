25a116fe33e8c8cd6d67b35482bf2cb5a9e1378b
Jacky Tung <jacky.tung@langlive.com>
2020-11-03 14:24:05 +0800

    init commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import AmazonIVSWorkaround from "./AmazonIVSWorkaround";
+
+function App() {
+    return (
+        <div className="App">
+            <AmazonIVSWorkaround/>
+        </div>
+    );
+}
+
+export default App;
